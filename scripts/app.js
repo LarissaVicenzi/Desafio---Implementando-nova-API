51bbf277c94f00abeb25b9f81d898b4fb9169ef3
Lucas <sergiorockemroll@gmail.com>
2025-09-24 23:57:36 -0300

    API Dog Funcinando

--- scripts/app.js
+++ scripts/app.js
@@ -1,31 +1,12 @@
-let button = document.getElementById('btnUsers');
-let div = document.getElementById('out');
+let buttonDog = document.getElementById("btnDog");
+let div = document.getElementById("out");
 
-button.addEventListener('click', function () {
-    // Assíncrona Async Chaining
-    fetch('https://jsonplaceholder.typicode.com/posts/101')
-    .then(function (response) {
-        if (response.ok) {
-            return response.json();
-        } else {
-            let errorMessage = "Recurso não encontrado ou servidor não disponível. Tente novamente mais tarde.";
-            throw new Error(errorMessage);
-        }
-    })
-    .then(function (json) {
-        let post = post.fromRaw(json);
-        post.renderFrom(div);
-    })
-    .catch(function (error) {
-        renderError(error);
-    })
+buttonDog.addEventListener("click", function () {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then(json => {
+      let dog = Dog.fromRaw(json);
+      dog.renderFrom(div);
+    });
 });
 
-function renderError(error) {
-    div.innerHTML = "";
-    
-    let h2 = document.createElement(`h2`);
-    h2.textContent = error;
-    
-    div.appendChild(h2);
-}
